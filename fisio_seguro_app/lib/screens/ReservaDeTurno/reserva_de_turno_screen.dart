5ad27da5e6a3b75feae6a1427ea2367ac067f5dd
Emilio Ginzo <emilioginzo@fpuna.edu.py>
2023-10-28 16:58:23 -0300

    la vista ahora muesta el paciente el doctor, fecha

--- fisio_seguro_app/lib/screens/ReservaDeTurno/reserva_de_turno_screen.dart
+++ fisio_seguro_app/lib/screens/ReservaDeTurno/reserva_de_turno_screen.dart
@@ -1,4 +1,11 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:intl/intl.dart';
+
 
 class ReservaDeTurnosScreen extends StatefulWidget {
   const ReservaDeTurnosScreen({super.key});
@@ -9,45 +16,86 @@ class ReservaDeTurnosScreen extends StatefulWidget {
 }
 
 class _ReservaDeTurnosScreenState extends State<ReservaDeTurnosScreen> {
-  String? selectedDoctor = '';
-  String? selectedPatient = '';
-  DateTime? selectedDate;
+  List<Map<String, dynamic>> persons = [];
+  DateTime selectedDate = DateTime.now();
   String? selectedTime = null;
+  // Controllers for form inputs
+  late String filePath;
+  TextEditingController idController = TextEditingController();
+  TextEditingController descripcionController = TextEditingController();
+  TextEditingController nameController = TextEditingController();
+  TextEditingController apellidoController = TextEditingController();
+  TextEditingController telefonoController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController cedulaController = TextEditingController();
+  TextEditingController isDoctorController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeFilePath();
+  }
+
+  Future<void> _savepersons() async {
+    final File file = File(filePath);
+    final String data = json.encode(persons);
+    await file.writeAsString(data);
+  }
+
+  Future<void> _initializeFilePath() async {
+    final directory = await getApplicationDocumentsDirectory();
+    filePath = '${directory.path}/persons.json';
+
+    final File file = File(filePath);
+    final String jsonString = await rootBundle.loadString('assets/persons.json');
+    await file.writeAsString(jsonString);
+    if (!await file.exists()) {
+      final String jsonString = await rootBundle.loadString('assets/persons.json');
+      await file.writeAsString(jsonString);
+    }
+
+    _loadpersons();
+  }
+
+  Future<void> _loadpersons() async {
+    if (filePath.isEmpty) return;
+
+    final File file = File(filePath);
+    final data = await file.readAsString();
+    final List<Map<String, dynamic>> loadedpersons = List.from(json.decode(data));
+    loadedpersons.sort((a, b) => a['idPersona'].compareTo(b['idPersona']));
+
+    setState(() {
+      persons = loadedpersons;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Reserva de Turno'),
-      ),
+      appBar: AppBar(title: const Text('Reserva de turnos')),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView(
+        child: Column(
           children: [
-            const Text('Doctor:'),
+            // Form for adding new persons (nombre, apellido, telefono, email, cedula, checkbox is doctor)
+            //make a form for the registration of a person
+            // start then the name, then the last name, then the phone number, then the email, then the cedula number, then the checkbox for if it is a doctor or not
+            TextField(
+              controller: nameController,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                labelText: 'Paciente',
+              ),
+            ),
             const SizedBox(height: 10),
-            const Text('Pacientes:'),
-            // DropdownButton<String?>(
-            //   value: selectedPatient,
-            //   hint: const Text('Selecciona un paciente'),
-            //   items: [
-            //     'David Wilson',
-            //     'Emily Jones',
-            //     // ... add other patients here
-            //   ]
-            //       .map((patient) => DropdownMenuItem(
-            //             value: patient,
-            //             child: Text(patient),
-            //           ))
-            //       .toList(),
-            //   onChanged: (value) {
-            //     setState(() {
-            //       selectedPatient = value ?? '';
-            //     });
-            //   },
-            // ),
+            TextField(
+              controller: apellidoController,
+              decoration: const InputDecoration(
+                labelText: 'Doctor',
+              ),
+            ),
             const SizedBox(height: 10),
-            const Text('Fecha:'),
             TextFormField(
               decoration: const InputDecoration(
                 hintText: 'Selecciona una fecha',
@@ -70,7 +118,6 @@ class _ReservaDeTurnosScreenState extends State<ReservaDeTurnosScreen> {
               readOnly: true,
             ),
             const SizedBox(height: 10),
-            const Text('Hora:'),
             DropdownButton<String>(
               value: selectedTime,
               hint: const Text('Selecciona una hora'),
@@ -102,34 +149,119 @@ class _ReservaDeTurnosScreenState extends State<ReservaDeTurnosScreen> {
               },
             ),
             ElevatedButton(
-              onPressed: () {},
-              child: const Text('Agregar Reserva'),
+              onPressed: _addCategory,
+              child: const Text('Agendar Reserva'),
             ),
-            const Divider(),
-            const Text('Reservas', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
-            DataTable(
-              columns: const [
-                DataColumn(label: Text('Doctor')),
-                DataColumn(label: Text('Paciente')),
-                DataColumn(label: Text('Fecha')),
-                DataColumn(label: Text('Hora')),
-                DataColumn(label: Text('Acción')),
-              ],
-              rows: [
-                // Example row, add more rows as needed
-                DataRow(cells: [
-                  const DataCell(Text('John Doe')),
-                  const DataCell(Text('Mia Foster')),
-                  const DataCell(Text('2023/10/27')),
-                  const DataCell(Text('12:30 - 13:00')),
-                  DataCell(ElevatedButton(onPressed: () {}, child: const Text('Cancelar')))
-                ]),
-              ],
-            )
+            const SizedBox(height: 20),
+            // Table to display persons
+            Expanded(
+              child: ListView.builder(
+                itemCount: persons.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text('Paciente: ${persons[index]['nombre']} ${persons[index]['apellido']}'),
+                    //subtitle = telefono, email, cedula
+                    subtitle: Text('Doctor: ${persons[index]['nombre']} ${persons[index]['apellido']}\nFecha: ${DateFormat('MM-dd-yyyy').format(selectedDate)}\nHora: ${selectedTime}'),
+                  );
+                },
+              ),
+            ),
           ],
         ),
       ),
     );
   }
 
+  void _addCategory() {
+    if (nameController.text.isNotEmpty && apellidoController.text.isNotEmpty && telefonoController.text.isNotEmpty && emailController.text.isNotEmpty && cedulaController.text.isNotEmpty && isDoctorController.text.isNotEmpty) {
+      int newId =  persons.isNotEmpty ? persons.last['idPersona'] + 1 : 1;
+      
+      setState(() {
+        persons.add({
+          'idPersona': newId,
+          'nombre': nameController.text,
+          'apellido': apellidoController.text,
+          'telefono': telefonoController.text,
+          'email': emailController.text,
+          'cedula': cedulaController.text,
+          'isDoctor': isDoctorController.text == 'true',
+        });
+        persons.sort((a, b) => a['idPersona'].compareTo(b['idPersona']));
+
+        idController.clear();
+        descripcionController.clear();
+      });
+
+      _savepersons(); // Save changes to file
+    }
+  }
+
+  void _editCategory(int index) {
+    final TextEditingController idEditController = TextEditingController(text: persons[index]['id'].toString());
+    final TextEditingController descripcionEditController = TextEditingController(text: persons[index]['descripcion']);
+
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Edit Category'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: idEditController,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                labelText: 'Id Categoría',
+              ),
+            ),
+            TextField(
+              controller: descripcionEditController,
+              decoration: const InputDecoration(
+                labelText: 'Descripción',
+              ),
+            ),
+          ],
+        ),
+        actions: [
+          ElevatedButton(
+            onPressed: () {
+              final int newId = int.parse(idEditController.text);
+              final String newDescripcion = descripcionEditController.text;
+
+              // Check if updated ID is unique (excluding the current category being edited)
+              if (persons.where((category) => category['idPersona'] == newId && persons.indexOf(category) != index).isNotEmpty) {
+                ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('ID already exists!')));
+                return;
+              }
+
+              setState(() {
+                persons[index] = {
+                  'idPersona': newId,
+                  'descripcion': newDescripcion,
+                };
+                persons.sort((a, b) => a['idPersona'].compareTo(b['idPersona']));
+              });
+
+              _savepersons(); // Save changes to file
+              Navigator.of(context).pop();
+            },
+            child: const Text('Update'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: const Text('Cancel'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _deleteCategory(int index) {
+    setState(() {
+      persons.removeAt(index);
+    });
+    _savepersons(); // Save changes to file
+  }
 }
